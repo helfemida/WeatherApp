a74d2258371d483a18a07064a08b8e5e69c2bbdf
Albina Yermek <yermekalbina@gmail.com>
2023-10-31 23:36:02 +0600

    2nd version

--- script.js
+++ script.js
@@ -1,66 +1,53 @@
-function getWeatherByCity(city) {
-    // Replace this section with code to fetch weather data based on the city
-    // This is a placeholder for fetching weather data using the city name
-    // Simulating weather data using random values
-    const mockWeatherData = {
-      name: city,
-      country: 'NA',
-      temp: Math.floor(Math.random() * 30),
-      description: 'Mock Weather Description',
-      humidity: Math.floor(Math.random() * 100),
-      windSpeed: Math.floor(Math.random() * 20)
-    };
-    displayWeather(mockWeatherData);
-  }
-  
-  function getCityNameFromOpenStreetMap(latitude, longitude) {
-    const apiUrl = `https://nominatim.openstreetmap.org/reverse?format=jsonv2&lat=${latitude}&lon=${longitude}`;
-  
-    fetch(apiUrl)
-      .then(response => response.json())
-      .then(data => {
-        const city = data.address.city || data.address.town || data.address.village || data.address.county || data.address.state || '';
-        if (city !== '') {
-          getWeatherByCity(city);
-        } else {
-          console.log('City not found');
-        }
-      })
-      .catch(error => console.log('Error fetching city name:', error));
-  }
-  
-  function getWeatherByCoordinates(latitude, longitude) {
-    getCityNameFromOpenStreetMap(latitude, longitude);
-  }
-  
-  function getWeather() {
-    const city = document.getElementById('city').value;
-    getWeatherByCity(city);
-  }
-  
-  function getWeatherByLocation() {
-    if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition((position) => {
-        const latitude = position.coords.latitude;
-        const longitude = position.coords.longitude;
-        getWeatherByCoordinates(latitude, longitude);
-      }, showError);
-    } else {
-      alert("Geolocation is not supported by this browser.");
+const container = document.querySelector('.container');
+const search = document.querySelector('.search-box button');
+const weatherBox = document.querySelector('.weather-box');
+const weatherDetails = document.querySelector('.weather-details');
+
+search.addEventListener('click', () => {
+
+  const APIkey = '56ea7cc381583a16e6e7bc493bace6ca';
+  const city = document.querySelector('.search-box input').value;
+
+  if(city == '')
+    return;
+  fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${APIkey}`).then(response => response.json()).then(json => {
+
+    const image = document.querySelector('.weather-box img');
+    const temperature = document.querySelector('.weather-box .temperature');
+    const description = document.querySelector('.weather-box .description');
+    const humidity = document.querySelector('.weather-details .humidity');
+    const wind = document.querySelector('.weather-details .wind');
+
+    switch(json.weather[0].main){
+      case 'Clear':
+        image.src = 'src/sun.png';
+        break;
+
+      case 'Rain':
+        image.src = 'src/rain.png';
+        break;
+      case 'Snow':
+        image.src = 'src/snowy.png';
+        break;
+      case 'Clouds':
+        image.src = 'src/cloudy.png';
+        break;
+      case 'Haze':
+        image.src = 'src/windy.png';
+        break;
+      case 'Mist':
+        image.src = 'src/windy.png';
+        break;
+      default:
+        image.src = 'images/sun.png';
+        break;
     }
-  }
-  
-  function showError(error) {
-    alert("Unable to retrieve your location.");
-  }
-  
-  function displayWeather(data) {
-    const weatherInfo = document.getElementById('weather-info');
-    weatherInfo.innerHTML = `
-      <h2>${data.name}, ${data.country}</h2>
-      <p>Temperature: ${data.temp}°C</p>
-      <p>Description: ${data.description}</p>
-      <p>Humidity: ${data.humidity}%</p>
-      <p>Wind Speed: ${data.windSpeed} m/s</p>
-    `;
-  }
+
+    temperature.innerHTML = `${parseInt(json.main.temp)}<span>C</span>`;
+    description.innerHTML = `${json.weather[0].description}`;
+    humidity.innerHTML = `${json.main.humidity}%`;
+    wind.innerHTML = `${parseInt(json.wind.speed)}Km/h`
+
+  });
+
+})
